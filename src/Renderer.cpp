38d22de8bfca15a45d9ccb4532b7909ea9a712fe
wootguy <drake.rhunter@gmail.com>
2020-05-22 05:16:36 -0700

    camera controls
    wishDir stuff also from old project which I think is a copy from Quake

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -38,6 +38,12 @@ Renderer::Renderer() {
 	glfwSetKeyCallback(window, key_callback);
 
 	glewInit();
+
+	pipeline = new ShaderProgram(g_shader_cVert_vertex, g_shader_cVert_fragment);
+	pipeline->setMatrixes(&model, &view, &projection, &modelView, &modelViewProjection);
+	pipeline->setMatrixNames(NULL, "modelViewProjection");
+	pipeline->setVertexAttributeNames("vPosition", "vColor", NULL);
+	pipeline->bind();
 }
 
 Renderer::~Renderer() {
@@ -47,44 +53,35 @@ Renderer::~Renderer() {
 void Renderer::renderLoop() {
 	glfwSwapInterval(1);
 
-	ShaderProgram pipeline = ShaderProgram(g_shader_cVert_vertex, g_shader_cVert_fragment);
-
-	mat4x4 model, view, projection, modelView, modelViewProjection;
-	pipeline.setMatrixes(&model, &view, &projection, &modelView, &modelViewProjection);
-	pipeline.setMatrixNames(NULL, "modelViewProjection");
-	pipeline.setVertexAttributeNames("vPosition", "vColor", NULL);
-	pipeline.bind();
-
 	cCube cube(vec3(-10, -10, -10), vec3(10, 10, 10), {0, 255, 0} );
 	cube.setColor({ 0,255,0 }, { 0,0,255 }, { 255,0,0 }, { 0,255,255 }, { 255,0,255 }, { 0,255,0 });
 	
-	VertexBuffer buffer(&pipeline, COLOR_3B | POS_3F, &cube, 6*6);
+	VertexBuffer buffer(pipeline, COLOR_3B | POS_3F, &cube, 6*6);
 
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 
+	cameraOrigin.y = -50;
+
+	float lastFrameTime = glfwGetTime();
 	while (!glfwWindowShouldClose(window))
 	{
-		float fov = 75.0f;
-		int width, height;
+		float frameDelta = glfwGetTime() - lastFrameTime;
+		frameTimeScale = 0.002f / frameDelta;
+		lastFrameTime = glfwGetTime();
 
-		glfwGetFramebufferSize(window, &width, &height);
+		cameraControls();
 
-		glViewport(0, 0, width, height);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		view.loadIdentity();
-		view.translate(0, 0, -50);
-
-		model.loadIdentity();
 		float spin = glfwGetTime() * 2;
+		model.loadIdentity();
 		model.rotateZ(spin);
 		model.rotateX(spin);
-
-		projection.perspective(fov, (float)width / (float)height, 1.0f, 4096.0f);
-
-		pipeline.updateMatrixes();
 		
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		setupView();
+		pipeline->updateMatrixes();
+
 		buffer.draw(GL_TRIANGLES);
 
 		glfwSwapBuffers(window);
@@ -94,6 +91,82 @@ void Renderer::renderLoop() {
 	glfwTerminate();
 }
 
+void Renderer::cameraControls() {
+	cameraOrigin += getMoveDir() * frameTimeScale;
+
+	double xpos, ypos;
+	glfwGetCursorPos(window, &xpos, &ypos);
+	vec2 mousePos(xpos, ypos);
+
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS) {
+		if (!cameraIsRotating) {
+			lastMousePos = mousePos;
+			cameraIsRotating = true;
+		}
+		else {
+			vec2 drag = mousePos - lastMousePos;
+			cameraAngles.z += drag.x * 0.5f;
+			cameraAngles.x += drag.y * 0.5f;
+			lastMousePos = mousePos;
+		}
+	}
+	else {
+		cameraIsRotating = false;
+	}
+}
+
+vec3 Renderer::getMoveDir()
+{
+	vec3 wishdir(0, 0, 0);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	{
+		wishdir.x += (float)-cos(PI * (cameraAngles.z) / 180.0f);
+		wishdir.y += (float)sin(PI * (cameraAngles.z) / 180.0f);
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+	{
+		wishdir.x -= (float)-cos(PI * (cameraAngles.z) / 180.0f);
+		wishdir.y -= (float)sin(PI * (cameraAngles.z) / 180.0f);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	{
+		wishdir.x += (float)-cos(PI * (cameraAngles.z + 90) / 180.0f);
+		wishdir.y += (float)sin(PI * (cameraAngles.z + 90) / 180.0f);
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+	{
+		wishdir.x -= (float)-cos(PI * (cameraAngles.z + 90) / 180.0f);
+		wishdir.y -= (float)sin(PI * (cameraAngles.z + 90) / 180.0f);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+		wishdir.z += 1;
+	if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
+		wishdir.z -= 1;
+	return wishdir;
+}
+
+void Renderer::setupView() {
+	float fov = 75.0f;
+	int width, height;
+
+	glfwGetFramebufferSize(window, &width, &height);
+
+	glViewport(0, 0, width, height);
+
+	projection.perspective(fov, (float)width / (float)height, 1.0f, 4096.0f);
+
+	view.loadIdentity();
+	view.rotateX(PI * cameraAngles.x / 180.0f);
+	view.rotateY(PI * cameraAngles.z / 180.0f);
+	view.translate(-cameraOrigin.x, -cameraOrigin.z, cameraOrigin.y);
+}
+
 void Renderer::addMap(Bsp* map) {
+	
+}
+
+void Renderer::renderMap(Bsp* bsp) {
 
 }
--- src/Renderer.h
+++ src/Renderer.h
@@ -1,6 +1,7 @@
 #include "Bsp.h"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include "gfx/ShaderProgram.h"
 
 class Renderer {
 public:
@@ -15,6 +16,21 @@ public:
 
 private:
 	GLFWwindow* window;
+	ShaderProgram* pipeline;
 
-	void render();
+	vec3 cameraOrigin;
+	vec3 cameraAngles;
+	bool cameraIsRotating;
+	float frameTimeScale = 0.0f;
+	mat4x4 model, view, projection, modelView, modelViewProjection;
+
+	
+
+	vec2 lastMousePos;
+
+	void renderMap(Bsp* bsp);
+
+	vec3 getMoveDir();
+	void cameraControls();
+	void setupView();
 };
